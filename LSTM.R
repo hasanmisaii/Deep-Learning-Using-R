f83b26f6695448c3fa69dfc10e53de445e065284
Hasan MISAII <hasanmisaii14@gmail.com>
2023-06-08 14:33:28 +0200

    Create LSTM.R
    Long Short Term Memory

---
+++ LSTM.R
@@ -0,0 +1,8 @@
+# change your environment to the python environment
+reticulate::use_condaenv("python.exe")
+
+
+#Install keras and tensorflow in the R terminal using pip and install.packages()
+library(keras)
+
+
